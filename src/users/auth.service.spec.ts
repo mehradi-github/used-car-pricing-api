60aae6f1f35198fb29d79f7b36a53714b4ef75fd
Mehradi <mehradi.info@gmail.com>
2022-11-07 10:56:32 +0200

    throwing an error if user signs up with email that is in use

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,21 +1,35 @@
+import { BadRequestException } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { User } from './user.entity';
 import { UsersService } from './users.service';
 
 describe('AuthService', () => {
   let authService: AuthService;
+  let fakeUsersService: Partial<UsersService>;
   beforeEach(async () => {
-    const fakeUserService = {
-      find: () => Promise.resolve([]),
-      create: (email: string, password: string) =>
-        Promise.resolve({ id: 1, email, password }),
+    const users: User[] = [];
+    fakeUsersService = {
+      find: (email: string) => {
+        const filteredUsers = users.filter((user) => user.email === email);
+        return Promise.resolve(filteredUsers);
+      },
+      create: (email: string, password: string) => {
+        const user = {
+          id: Math.floor(Math.random() * 999999),
+          email,
+          password,
+        } as User;
+        users.push(user);
+        return Promise.resolve(user);
+      },
     };
     const module = await Test.createTestingModule({
       providers: [
         AuthService,
         {
           provide: UsersService,
-          useValue: fakeUserService,
+          useValue: fakeUsersService,
         },
       ],
     }).compile();
@@ -31,5 +45,30 @@ describe('AuthService', () => {
     const [salt, hash] = user.password.split('.');
     expect(salt).toBeDefined();
     expect(hash).toBeDefined();
+  });
+
+  //   it('throws an error if user signup with email that in use', async (done) => {
+  //     fakeUserService.find = () =>
+  //       Promise.resolve([
+  //         {
+  //           id: 1,
+  //           email: 'test@test.com',
+  //           password: 'test',
+  //         } as User,
+  //       ]);
+  //     try {
+  //       await authService.signup('test@test.com', 'test');
+  //     } catch (err) {
+  //       done();
+  //     }
+  //   });
+
+  it('throws an error if user signs up with email that is in use', async () => {
+    await authService.signup('asdf@asdf.com', 'asdf');
+    try {
+      await authService.signup('asdf@asdf.com', 'asdf');
+    } catch (e) {
+      expect(e.toString()).toMatch('email in use');
+    }
   });
 });
