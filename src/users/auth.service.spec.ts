c781fba6104876fc32b62d54d9a75c09b1d2d04f
Mehradi <mehradi.info@gmail.com>
2022-11-07 07:07:47 +0200

    Testing to create a new user with salt and hashed password

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,25 +1,35 @@
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
-let authService: AuthService;
-beforeEach(async () => {
-  const fakeUserService = {
-    find: () => Promise.resolve([]),
-    create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password }),
-  };
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      {
-        provide: UsersService,
-        useValue: fakeUserService,
-      },
-    ],
-  }).compile();
 
-  authService = module.get(AuthService);
-});
-it('can create instance of auth service', async () => {
-  expect(authService).toBeDefined();
+describe('AuthService', () => {
+  let authService: AuthService;
+  beforeEach(async () => {
+    const fakeUserService = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password }),
+    };
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUserService,
+        },
+      ],
+    }).compile();
+
+    authService = module.get(AuthService);
+  });
+  it('can create instance of auth service', async () => {
+    expect(authService).toBeDefined();
+  });
+  it('create a new user with salt and hashed password', async () => {
+    const user = await authService.signup('test@test.com', 'asdf');
+    expect(user.password).not.toEqual('asdf');
+    const [salt, hash] = user.password.split('.');
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  });
 });
