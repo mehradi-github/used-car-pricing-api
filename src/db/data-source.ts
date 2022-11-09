530afef1a8d6ca0806aab0480a92b675e7ce9984
Mehradi <mehradi.info@gmail.com>
2022-11-09 22:18:46 +0200

    Preparing production for deployment

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -30,6 +30,17 @@ switch (process.env.NODE_ENV) {
     };
     break;
   case 'production':
+    dataSourceOptions = {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      entities: ['**/*.entity.js'],
+      migrations: ['migrations/*.js'],
+      synchronize: false,
+      migrationsRun: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    };
     break;
   default:
     throw new Error('unknown environment');
