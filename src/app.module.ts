fba9248d33cce86809227ef62b9b1e11ae9b6eb9
Mehradi <mehradi.info@gmail.com>
2022-11-09 16:35:43 +0200

    Calling config from configService

--- src/app.module.ts
+++ src/app.module.ts
@@ -50,11 +50,12 @@ const cookieSession = require('cookie-session');
   ],
 })
 export class AppModule {
+  constructor(private configService: ConfigService) {}
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(
         cookieSession({
-          keys: ['asdfgsfg'],
+          keys: [this.configService.get('COOKIE_KEY')],
         }),
       )
       .forRoutes('*');
